840157933d1f712e33d03b42102fbe07ed887fd1
Celik Koseoglu <celikkoseoglu@yahoo.com>
2017-05-12 04:02:08 +0300

    new er diagram added

--- carshare-today-node/server.js
+++ carshare-today-node/server.js
@@ -3,13 +3,12 @@ var connection = mysql.createConnection({
   host     : 'dijkstra2.ug.bcc.bilkent.edu.tr',
   user     : 'celik.koseoglu',
   password : '3slndceo',
-  database : 'celik_koseoglu'
+  database : 'test'
 });
-
-
 
 var express = require('express');
 var fs = require('fs');
+var bodyParser = require('body-parser');
 
 var app = express();
 
@@ -22,20 +21,39 @@ app.use(function(req, res, next) {
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+/**bodyParser.json(options)
+ * Parses the text as JSON and exposes the resulting object on req.body.
+ */
+app.use(bodyParser.json());
+
+app.post("/", function (req, res) {
+    console.log(req.body.user.name);
+    console.log(req.body.user.email);
+
+    connection.connect();
+
+    var query = "INSERT INTO USER (ID, email, password, fname, lname, phone_num, age, rating, member_since, gender, car_license_plate, bank_account, smokes, chattiness) Values(321321321, \"asdasd@gmail.com\", \"asdqwe123\", \"" + req.body.user.name + "\", \"yldmr\", \"053421233\", 21, 5.0, \"2017\", \"boy\", \"06BRN123\", \"IBAN123123\", 0, 3)"
+
+    connection.query(query, function (error, results, fields) {
+       if (error)
+         throw error;
+    });
+
+    connection.end();
+
+    res.end();
+});
+
 app.get('/', function(request, response) {
-
   	response.render('pages/index');
-
-  	connection.connect();
-
-	connection.query('SELECT * FROM customer', function (error, results, fields) {
-	  if (error)
-	 	throw error;
-	  res.send(results[0].name);
-	});
-
-	connection.end();
-
 });
 
 app.listen(3001, function () {
