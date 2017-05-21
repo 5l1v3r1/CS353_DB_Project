1a4f7523a893411be1fbc45d6b6c9113a5899850
celikkoseoglu <celikkoseoglu@yahoo.com>
2017-05-21 03:59:20 +0300

    nodejs user sessions

--- carshare-today-node/server.js
+++ carshare-today-node/server.js
@@ -1,16 +1,17 @@
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'dijkstra2.ug.bcc.bilkent.edu.tr',
-  user     : 'celik.koseoglu',
-  password : '3slndceo',
-  database : 'test'
-});
-
 var express = require('express');
-var fs = require('fs');
+var session = require('express-session');
 var bodyParser = require('body-parser');
-
 var app = express();
+
+app.use(express.static(__dirname + '/views'));
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+
+app.use(session({secret: 'ssshhhhh'}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -18,44 +19,85 @@ app.use(function(req, res, next) {
     next();
 });
 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+var sess;
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.get('/', function (req, res) {
+    sess = req.session;
+    if (sess.email) { //check if the previous user has logged out or not. If not, then display personal page.
+        res.redirect('/admin');
+    }
+    else {
+        res.render('pages/login');
+    }
+});
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.get('/register', function (req, res) {
+    res.render('pages/register');
+});
 
-/**bodyParser.json(options)
- * Parses the text as JSON and exposes the resulting object on req.body.
- */
-app.use(bodyParser.json());
-
-app.post("/", function (req, res) {
-    console.log(req.body.user.name);
-    console.log(req.body.user.email);
-
-    connection.connect();
-
-    var query = "INSERT INTO USER (ID, email, password, fname, lname, phone_num, age, rating, member_since, gender, car_license_plate, bank_account, smokes, chattiness) Values(321321321, \"asdasd@gmail.com\", \"asdqwe123\", \"" + req.body.user.name + "\", \"yldmr\", \"053421233\", 21, 5.0, \"2017\", \"boy\", \"06BRN123\", \"IBAN123123\", 0, 3)"
-
-    connection.query(query, function (error, results, fields) {
-       if (error)
-         throw error;
+app.get('/logout', function (req, res) {
+    req.session.destroy(function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect('/');
+        }
     });
+});
 
-    connection.end();
+app.get('/admin', function (req, res) {
+    sess = req.session;
+    if (sess.email) {
+        res.write('<h1>Hello ' + sess.email + '</h1>');
+        res.end('<a href="/logout">Logout</a>');
+    } else {
+        res.write('<h1>Please login first.</h1>');
+        res.end('<a href="/">Login</a>');
+    }
+});
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+    host     : 'dijkstra2.ug.bcc.bilkent.edu.tr',
+    user     : 'celik.koseoglu',
+    password : '3slndceo',
+    database : 'celik_koseoglu'
+});
+
+
+app.post("/login", function (req, res) {
+
+    sess = req.session;
+    sess.email = req.body.user.email;
+    sess.password = req.body.user.password;
+
+    console.log("email: " + req.body.user.email);
+
+    var getPasswordQuery = "SELECT password FROM USER WHERE email=\"" + req.body.user.email + "\"";
+
+    connection.query(getPasswordQuery, function (error, results) {
+        if (error)
+            throw error;
+        if(results.length > 0 &&  results[0].password == sess.password)
+            res.redirect('/admin');
+        else {
+            res.end();
+        }
+    });
+});
+
+app.post("/register", function (req, res) {
+
+    var registerUserQuery = "INSERT INTO USER (ID, email, password, fname, lname, phone_num, age, rating, member_since, gender, car_license_plate, bank_account, smokes, chattiness) Values(321321321, \""+ req.body.user.email +"\", \""+ req.body.user.password +"\", \"" + req.body.user.name + "\", \""+ req.body.user.surname +"\", \"053421233\", 21, 5.0, \"2017\", \"boy\", \"06BRN123\", \"IBAN123123\", 0, 3)"
+
+    connection.query(registerUserQuery, function (error, results, fields) {
+        if (error)
+            throw error;
+    });
 
     res.end();
 });
 
-app.get('/', function(request, response) {
-  	response.render('pages/index');
-});
-
-app.listen(3001, function () {
-    console.log('Example app listening on port 3001!');
+app.listen(3000, function () {
+    console.log("App Started on PORT 3000");
 });
